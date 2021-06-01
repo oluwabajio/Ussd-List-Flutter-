ef72aa190ba7d69157df94deb771f7a3f7ab8ef7
oluwabajio <ismailosunlana@yahoo.com>
2021-05-31 21:00:31 -0500

    Added admob

--- lib/ad_helper.dart
+++ lib/ad_helper.dart
@@ -3,9 +3,10 @@ import 'dart:io';
 class AdHelper {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-3940256099942544/6300978111';
+      return 'ca-app-pub-3940256099942544/6300978111'; //test
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-3940256099942544/2934735716';
+     return 'ca-app-pub-2439901986027384/2580537828'; //live
+     // return 'ca-app-pub-3940256099942544/2934735716'; //test
     } else {
       throw new UnsupportedError('Unsupported platform');
     }
