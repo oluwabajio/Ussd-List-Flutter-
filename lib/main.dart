497c71598b80d4e6ed9c2077ec3120e440dacdf7
oluwabajio <ismailosunlana@yahoo.com>
2021-05-31 20:39:41 -0500

    Added admob

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:nigerian_ussd_codes/codeList.dart';
-import 'package:nigerian_ussd_codes/ussdCode.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'dart:async';
+import 'dart:io';
+
+import 'ad_helper.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,31 +14,25 @@ void main() {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 4,
-        child: MaterialApp(
-          title: 'Flutter Demo',
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-          ),
-          home: MyHomePage(title: 'Nigerian Ussd Codes'),
-        ));
+      length: 4,
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage(title: 'Nigerian Ussd Codes'),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -44,14 +43,114 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  // TODO: Add _bannerAd
+  BannerAd _bannerAd;
+  BannerAd _bannerAd2;
+  BannerAd _bannerAd3;
+  BannerAd _bannerAd4;
+
+// TODO: Add _isBannerAdReady
+  bool _isBannerAdReady = false;
+  bool _isBannerAdReady2 = false;
+  bool _isBannerAdReady3 = false;
+  bool _isBannerAdReady4 = false;
+
+  @override
+  void initState() {
+    super.initState();
+    // Load ads.
+    print('initstate called');
+    MobileAds.instance.initialize();
+
+    _bannerAd = BannerAd(
+      adUnitId: AdHelper.bannerAdUnitId,
+      request: AdRequest(),
+      size: AdSize.banner,
+      listener: AdListener(
+        onAdLoaded: (_) {
+          setState(() {
+            _isBannerAdReady = true;
+          });
+        },
+        onAdFailedToLoad: (ad, err) {
+          print('Failed to load a banner ad: ${err.message}');
+          _isBannerAdReady = false;
+          ad.dispose();
+        },
+      ),
+    );
+    _bannerAd.load();
+
+
+
+
+    _bannerAd2 = BannerAd(
+      adUnitId: AdHelper.bannerAdUnitId,
+      request: AdRequest(),
+      size: AdSize.banner,
+      listener: AdListener(
+        onAdLoaded: (_) {
+          setState(() {
+            _isBannerAdReady2 = true;
+            print('banner 2 loaded');
+          });
+        },
+        onAdFailedToLoad: (ad, err) {
+          print('Failed to load a banner ad: ${err.message}');
+          _isBannerAdReady2 = false;
+          ad.dispose();
+        },
+      ),
+    );
+    _bannerAd2.load();
+
+    _bannerAd3 = BannerAd(
+      adUnitId: AdHelper.bannerAdUnitId,
+      request: AdRequest(),
+      size: AdSize.banner,
+      listener: AdListener(
+        onAdLoaded: (_) {
+          setState(() {
+            _isBannerAdReady3 = true;
+            print('banner 3 loaded');
+          });
+        },
+        onAdFailedToLoad: (ad, err) {
+          print('Failed to load a banner ad: ${err.message}');
+          _isBannerAdReady3 = false;
+          ad.dispose();
+        },
+      ),
+    );
+    _bannerAd3.load();
+
+     _bannerAd4 = BannerAd(
+      adUnitId: AdHelper.bannerAdUnitId,
+      request: AdRequest(),
+      size: AdSize.banner,
+      listener: AdListener(
+        onAdLoaded: (_) {
+          setState(() {
+            _isBannerAdReady4 = true;
+            print('banner 4 loaded');
+          });
+        },
+        onAdFailedToLoad: (ad, err) {
+          print('Failed to load a banner ad: ${err.message}');
+          _isBannerAdReady4 = false;
+          ad.dispose();
+        },
+      ),
+    );
+    _bannerAd4.load();
+
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    print('build called');
+
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -90,21 +189,86 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      body: TabBarView(
-        children: [
-          Center(
-            child: mtnListView(),
-          ),
-          Center(
-            child: airtelListView(),
-          ),
-          Center(
-            child: ninemobileListView(),
-          ),
-          Center(
-            child: gloListView(),
-          ),
-        ],
+      body: FutureBuilder<void>(
+          future: _initGoogleMobileAds(),
+          builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
+            return TabBarView(
+              children: [
+                Center(
+                  child: Column(
+                    children: [
+                      if (_isBannerAdReady)
+                      showBannerAds(
+                          height: _bannerAd.size.height.toDouble(),
+                          width: _bannerAd.size.width.toDouble(),
+                          bannerAd: _bannerAd),
+                      Expanded(child: mtnListView()),
+                    ],
+                  ),
+                ),
+                Center(
+                  child: Column(
+                    children: [
+                      if (_isBannerAdReady2)
+                      showBannerAds(
+                          height: _bannerAd2.size.height.toDouble(),
+                          width: _bannerAd2.size.width.toDouble(),
+                          bannerAd: _bannerAd2),
+                      Expanded(child: airtelListView()),
+                    ],
+                  ),
+                ),
+                Center(
+                  child: Column(
+                    children: [
+                      if (_isBannerAdReady3)
+                      showBannerAds(
+                          height: _bannerAd3.size.height.toDouble(),
+                          width: _bannerAd3.size.width.toDouble(),
+                          bannerAd: _bannerAd3),
+                      Expanded(child: ninemobileListView()),
+                    ],
+                  ),
+                ),
+                Center(
+                  child: Column(
+                    children: [  if (_isBannerAdReady4)
+                      showBannerAds(
+                          height: _bannerAd4.size.height.toDouble(),
+                          width: _bannerAd4.size.width.toDouble(),
+                          bannerAd: _bannerAd4),
+                      Expanded(child: gloListView()),
+                    ],
+                  ),
+                ),
+              ],
+            );
+          }),
+    );
+  }
+
+  Future<InitializationStatus> _initGoogleMobileAds() {
+    // TODO: Initialize Google Mobile Ads SDK
+    return MobileAds.instance.initialize();
+  }
+}
+
+class showBannerAds extends StatelessWidget {
+  final double width;
+  final double height;
+  final BannerAd bannerAd;
+
+  const showBannerAds({Key key, this.width, this.height, this.bannerAd})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: Alignment.topCenter,
+      child: Container(
+        width: width,
+        height: height,
+        child: AdWidget(ad: bannerAd),
       ),
     );
   }
@@ -126,25 +290,42 @@ class mtnListView extends StatelessWidget {
               child: ListTile(
                 leading: Image.asset('assets/mtn_logo.png'),
                 title: Text(titles[index]),
-                subtitle: Text(subTitles[index]),
+                subtitle: Container(
+                  child: Text(
+                    subTitles[index],
+                    style: TextStyle(
+                      color: Colors.yellow[900],
+                      fontSize: 18,
+                    ),
+                  ),
+                  margin: EdgeInsets.only(top: 10.0),
+                ),
                 trailing: OutlinedButton(
                   child: Text('Dial'),
                   style: OutlinedButton.styleFrom(primary: Colors.black),
                   onPressed: () {
                     var telToDial = subTitles[index];
-                    launch("tel:$telToDial");
-                    ;
+                    telToDial = telToDial.substring(0, telToDial.length - 1);
+                    launchURL("tel:$telToDial" + "%23");
                   },
                 ),
                 onTap: () {
                   var telToDial = subTitles[index];
-                  launch("tel:$telToDial");
+                  telToDial = telToDial.substring(0, telToDial.length - 1);
+                  launchURL("tel:$telToDial" + Uri.encodeComponent("#"));
                 },
               ),
             ),
           );
         });
   }
+}
+
+void launchURL(String telToDial) async {
+  print(telToDial);
+  await canLaunch(telToDial)
+      ? await launch(telToDial)
+      : throw 'Could not launch $telToDial';
 }
 
 class airtelListView extends StatelessWidget {
@@ -163,19 +344,30 @@ class airtelListView extends StatelessWidget {
               child: ListTile(
                 leading: Image.asset('assets/airtel_logo.png'),
                 title: Text(titles[index]),
-                subtitle: Text(subTitles[index]),
+                subtitle: Container(
+                  child: Text(
+                    subTitles[index],
+                    style: TextStyle(
+                      color: Colors.red,
+                      fontSize: 18,
+                    ),
+                  ),
+                  margin: EdgeInsets.only(top: 10.0),
+                ),
                 trailing: OutlinedButton(
                   child: Text('Dial'),
                   style: OutlinedButton.styleFrom(primary: Colors.black),
                   onPressed: () {
                     var telToDial = subTitles[index];
-                    launch("tel:$telToDial");
+                    telToDial = telToDial.substring(0, telToDial.length - 1);
+                    launchURL("tel:$telToDial" + Uri.encodeComponent("#"));
                     ;
                   },
                 ),
                 onTap: () {
                   var telToDial = subTitles[index];
-                  launch("tel:$telToDial");
+                  telToDial = telToDial.substring(0, telToDial.length - 1);
+                  launchURL("tel:$telToDial" + Uri.encodeComponent("#"));
                 },
               ),
             ),
@@ -201,19 +393,30 @@ class ninemobileListView extends StatelessWidget {
               child: ListTile(
                 leading: Image.asset('assets/nine_mobile_logo.png'),
                 title: Text(titles[index]),
-                subtitle: Text(subTitles[index]),
+                subtitle: Container(
+                  child: Text(
+                    subTitles[index],
+                    style: TextStyle(
+                      color: Colors.green,
+                      fontSize: 18,
+                    ),
+                  ),
+                  margin: EdgeInsets.only(top: 10.0),
+                ),
                 trailing: OutlinedButton(
                   child: Text('Dial'),
                   style: OutlinedButton.styleFrom(primary: Colors.black),
                   onPressed: () {
                     var telToDial = subTitles[index];
-                    launch("tel:$telToDial");
+                    telToDial = telToDial.substring(0, telToDial.length - 1);
+                    launchURL("tel:$telToDial" + Uri.encodeComponent("#"));
                     ;
                   },
                 ),
                 onTap: () {
                   var telToDial = subTitles[index];
-                  launch("tel:$telToDial");
+                  telToDial = telToDial.substring(0, telToDial.length - 1);
+                  launchURL("tel:$telToDial" + Uri.encodeComponent("#"));
                 },
               ),
             ),
@@ -238,19 +441,29 @@ class gloListView extends StatelessWidget {
               child: ListTile(
                 leading: Image.asset('assets/glo_logo.png'),
                 title: Text(titles[index]),
-                subtitle: Text(subTitles[index]),
+                subtitle: Container(
+                  child: Text(
+                    subTitles[index],
+                    style: TextStyle(
+                      color: Colors.green,
+                      fontSize: 18,
+                    ),
+                  ),
+                  margin: EdgeInsets.only(top: 10.0),
+                ),
                 trailing: OutlinedButton(
                   child: Text('Dial'),
                   style: OutlinedButton.styleFrom(primary: Colors.black),
                   onPressed: () {
                     var telToDial = subTitles[index];
-                    launch("tel:$telToDial");
-                    ;
+                    telToDial = telToDial.substring(0, telToDial.length - 1);
+                    launchURL("tel:$telToDial" + Uri.encodeComponent("#"));
                   },
                 ),
                 onTap: () {
                   var telToDial = subTitles[index];
-                  launch("tel:$telToDial");
+                  telToDial = telToDial.substring(0, telToDial.length - 1);
+                  launchURL("tel:$telToDial" + Uri.encodeComponent("#"));
                 },
               ),
             ),
